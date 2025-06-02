cebfce598c2c2652fe1ba0d509201cc5d7380c12
Leslie Atterbury <atterburyleslie@gmail.com>
2025-06-02 15:32:30 -0700

    Implemented add and validation. Added tests for add and validation.

--- server/src/main/java/org/musicboxd/domain/ReviewService.java
+++ server/src/main/java/org/musicboxd/domain/ReviewService.java
@@ -26,4 +26,43 @@ public class ReviewService {
     public List<Review> findByUserId(int reviewerId, int currentUserId) {
         return repository.findByUserId(reviewerId, currentUserId);
     }
+
+    public Result<Review> add(Review review) {
+        Result<Review> result = validate(review);
+
+        if (result.getType() != ResultType.SUCCESS) {
+            return result;
+        }
+
+        Review added = repository.add(review);
+        result.setPayload(added);
+        return result;
+    }
+
+    private Result<Review> validate(Review review) {
+        Result<Review> result = new Result<>();
+
+        if (review == null) {
+            result.addMessage("Review cannot be null.", ResultType.INVALID);
+            return result;
+        }
+
+        if (review.getUserId() <= 0) {
+            result.addMessage("User ID is required.", ResultType.INVALID);
+        }
+
+        if (review.getAlbumId() <= 0) {
+            result.addMessage("Album ID is required.", ResultType.INVALID);
+        }
+
+        if (review.getContent() == null || review.getContent().isBlank()) {
+            result.addMessage("Content in review is required.", ResultType.INVALID);
+        }
+
+        if (review.getStars() < 1 || 5 < review.getStars()) {
+            result.addMessage("Stars must be between 1 and 5 inclusive.", ResultType.INVALID);
+        }
+
+        return result;
+    }
 }
--- server/src/test/java/org/musicboxd/domain/ReviewServiceTest.java
+++ server/src/test/java/org/musicboxd/domain/ReviewServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -55,6 +56,97 @@ class ReviewServiceTest {
 
         assertNotNull(all);
         assertEquals(2, all.size());
+    }
+
+    /* Add */
+
+    @Test
+    public void shouldAdd() {
+        Review expected = makeReview();
+        expected.setReviewId(2);
+
+        when(repository.add(any())).thenReturn(expected);
+
+        Result<Review> result = service.add(makeReview());
+
+        assertEquals(ResultType.SUCCESS, result.getType());
+        assertEquals(2, result.getPayload().getReviewId());
+    }
+
+    /* Validation */
+
+    @Test
+    public void shouldNotAddNullReview() {
+        Result<Review> result = service.add(null);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("null"));
+    }
+
+    @Test
+    public void shouldNotAddBadUserId() {
+        Review bad = makeReview();
+        bad.setUserId(-1);
+
+        Result<Review> result = service.add(bad);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("User ID"));
+    }
+
+    @Test
+    public void shouldNotAddBadAlbumId() {
+        Review bad = makeReview();
+        bad.setAlbumId(-1);
+
+        Result<Review> result = service.add(bad);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("Album ID"));
+    }
+
+    @Test
+    public void shouldNotAddNullContent() {
+        Review bad = makeReview();
+        bad.setContent(null);
+
+        Result<Review> result = service.add(bad);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("required"));
+    }
+
+    @Test
+    public void shouldNotAddEmptyContent() {
+        Review bad = makeReview();
+        bad.setContent(" ");
+
+        Result<Review> result = service.add(bad);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("required"));
+    }
+
+    @Test
+    public void shouldNotAddLowStars() {
+        Review bad = makeReview();
+        bad.setStars(-1);
+
+        Result<Review> result = service.add(bad);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("between"));
+    }
+
+    @Test
+    public void shouldNotAddHighStars() {
+        Review bad = makeReview();
+        bad.setStars(6);
+
+        Result<Review> result = service.add(bad);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("between"));
     }
 
     private Review makeReview() {
