f9c30b10cf5def615f856aeea0acfab6d23c90f1
Leslie Atterbury <atterburyleslie@gmail.com>
2025-06-02 09:47:55 -0700

    Added mappers for album and review.

--- server/src/main/java/org/musicboxd/data/mappers/AlbumMapper.java
+++ server/src/main/java/org/musicboxd/data/mappers/AlbumMapper.java
@@ -1,4 +1,26 @@
 package org.musicboxd.data.mappers;
 
-public class AlbumMapper {
+import org.musicboxd.models.Album;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class AlbumMapper implements RowMapper<Album> {
+
+    @Override
+    public Album mapRow(ResultSet resultSet, int i) throws SQLException {
+
+        Album album = new Album();
+        album.setAlbumId(resultSet.getInt("album_id"));
+        album.setArtist(resultSet.getString("artist"));
+        album.setId(resultSet.getString("id"));
+        album.setFirstReleaseDate(resultSet.getDate("first_released_date"));
+        album.setTitle(resultSet.getString("title"));
+        album.setArtUrl(resultSet.getString("art_url"));
+        album.setReviews(null);
+        return album;
+    }
 }
