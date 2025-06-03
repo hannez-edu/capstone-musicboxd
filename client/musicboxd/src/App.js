48d319d36bfaac5ad5aa1b807603f6f3da5045e4
bk <35516015+brendankrafty@users.noreply.github.com>
2025-06-03 00:38:38 -0400

    Add Navbar.js and NotFound.js, style basic navbar

--- client/musicboxd/src/App.js
+++ client/musicboxd/src/App.js
@@ -1,6 +1,14 @@
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Navbar";
+
 function App() {
   return (
-    <></>
+    <Router>
+      <Navbar />
+      <Routes>
+        
+      </Routes>
+    </Router>
   );
 }
 
