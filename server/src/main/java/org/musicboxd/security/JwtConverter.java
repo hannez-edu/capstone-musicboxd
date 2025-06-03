6851ede3b5fc0987d1a5d89dc49e41070c072c13
hannez-edu <hannez@uw.edu>
2025-06-02 20:49:03 -0700

    initial untested & unintegrated security component implementations

--- server/src/main/java/org/musicboxd/security/JwtConverter.java
+++ server/src/main/java/org/musicboxd/security/JwtConverter.java
@@ -1,4 +1,65 @@
 package org.musicboxd.security;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class JwtConverter {
+    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    private final String ISSUER = "musicboxd";
+    private final int EXPIRATION_MINUTES = 15;
+    private final int EXPIRATION_MILLIS = EXPIRATION_MINUTES * 60 * 1000;
+
+    public String getTokenFromUser(User user) {
+        String authorities = user.getAuthorities().stream()
+                .map(i -> i.getAuthority())
+                .collect(Collectors.joining(","));
+
+        return Jwts.builder()
+                .setIssuer(ISSUER)
+                .setSubject(user.getUsername())
+                .claim("authorities", authorities)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MILLIS))
+                .signWith(key)
+                .compact();
+    }
+
+    public User getUserFromToken(String token) {
+        if (token == null || !token.startsWith("Bearer ")) {
+            return null;
+        }
+
+        try {
+            Jws<Claims> jws = Jwts.parserBuilder()
+                    .requireIssuer(ISSUER)
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token.substring(7)); // Skip "Bearer " in token
+
+            String username = jws.getBody().getSubject();
+            String authStr = (String) jws.getBody().get("authorities");
+
+            List<GrantedAuthority> authorities = Arrays.stream(authStr.split(","))
+                    .map(i -> new SimpleGrantedAuthority(i))
+                    .collect(Collectors.toList());
+
+            // TODO: Ensure we don't need to somehow get a password here instead of the 2nd username specification.
+            return new User(username, username, authorities);
+        } catch (JwtException e) {
+            System.out.println(e);
+        }
+
+        return null;
+    }
 }
