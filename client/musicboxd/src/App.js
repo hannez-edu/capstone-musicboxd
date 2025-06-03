2c01faef92be2506a4ab3c11716bea76d8211bb0
Leslie Atterbury <atterburyleslie@gmail.com>
2025-06-03 11:40:17 -0700

    Created a basic album page.

--- client/musicboxd/src/App.js
+++ client/musicboxd/src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Navbar";
+import AlbumPage from "./AlbumPage";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        
+        <Route path="/album/:id" element={<AlbumPage />} />
       </Routes>
     </Router>
   );
