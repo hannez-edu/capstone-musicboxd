a01ddbd85a00bea49ca5cba80b4c265a842edb7a
hannez-edu <hannez@uw.edu>
2025-06-03 09:34:46 -0700

    login & register files

---
+++ client/musicboxd/src/Login.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function Login() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Login;
---
+++ client/musicboxd/src/Register.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+function Register() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Register;
