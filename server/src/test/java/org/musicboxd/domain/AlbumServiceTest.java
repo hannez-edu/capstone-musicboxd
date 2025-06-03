661f3f1ad1e7dc0f74c97f84c7924c4f8748258a
Leslie Atterbury <atterburyleslie@gmail.com>
2025-06-02 22:05:27 -0700

    Implemented add for albums. Implemented validation for albums. Added tests.

--- server/src/test/java/org/musicboxd/domain/AlbumServiceTest.java
+++ server/src/test/java/org/musicboxd/domain/AlbumServiceTest.java
@@ -11,6 +11,7 @@ import java.sql.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -53,6 +54,97 @@ public class AlbumServiceTest {
         Album album = service.findById(1, 1);
 
         assertNull(album);
+    }
+
+    /* Add */
+
+    @Test
+    public void shouldAdd() {
+        Album expected = makeAlbum();
+        expected.setAlbumId(1);
+
+        when(repository.add(any())).thenReturn(expected);
+
+        Result<Album> result = service.add(makeAlbum());
+
+        assertEquals(ResultType.SUCCESS, result.getType());
+        assertEquals(expected.getAlbumId(), result.getPayload().getAlbumId());
+    }
+
+    /* Validation */
+
+    @Test
+    public void shouldNotAddNull() {
+        Result<Album> result = service.add(null);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("null"));
+    }
+
+    @Test
+    public void shouldNotAddNullArtist() {
+        Album bad = makeAlbum();
+        bad.setArtist(null);
+
+        Result<Album> result = service.add(bad);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("required"));
+    }
+
+    @Test
+    public void shouldNotAddEmptyArtist() {
+        Album bad = makeAlbum();
+        bad.setArtist(" ");
+
+        Result<Album> result = service.add(bad);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("required"));
+    }
+
+    @Test
+    public void shouldNotAddNullTitle() {
+        Album bad = makeAlbum();
+        bad.setTitle(null);
+
+        Result<Album> result = service.add(bad);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("required"));
+    }
+
+    @Test
+    public void shouldNotAddEmptyTitle() {
+        Album bad = makeAlbum();
+        bad.setTitle(" ");
+
+        Result<Album> result = service.add(bad);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("required"));
+    }
+
+    @Test
+    public void shouldNotAddNullReleaseDate() {
+        Album bad = makeAlbum();
+        bad.setFirstReleaseDate(null);
+
+        Result<Album> result = service.add(bad);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("required"));
+    }
+
+    @Test
+    public void shouldNotAddFutureReleaseDate() {
+        Album bad = makeAlbum();
+        bad.setFirstReleaseDate(Date.valueOf("9999-01-01"));
+
+        Result<Album> result = service.add(bad);
+
+        assertEquals(ResultType.INVALID, result.getType());
+        assertTrue(result.getMessages().get(0).contains("future"));
     }
 
     private Album makeAlbum() {
