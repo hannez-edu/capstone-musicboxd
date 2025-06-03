2b9789e4c5e051c230269b0fd3558c95e3ca0893
hannez-edu <hannez@uw.edu>
2025-06-02 17:17:13 -0700

    implemented untested UserController

--- server/src/main/java/org/musicboxd/controllers/UserController.java
+++ server/src/main/java/org/musicboxd/controllers/UserController.java
@@ -1,5 +1,67 @@
 package org.musicboxd.controllers;
 
+import org.musicboxd.domain.Result;
+import org.musicboxd.domain.ResultType;
+import org.musicboxd.domain.UserService;
+import org.musicboxd.models.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/user")
 public class UserController {
-    // TODO
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("{userId}")
+    public User findById(@PathVariable int userId) {
+        return service.findById(userId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody User user) {
+        Result<User> result = service.add(user);
+
+        if (result.getType() == ResultType.SUCCESS) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping("{userId}")
+    public ResponseEntity<Object> update(@PathVariable int userId, @RequestBody User user) {
+        if (userId != user.getUserId()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        Result<User> result = service.update(user);
+        if (result.getType() == ResultType.SUCCESS) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("{userId}")
+    public ResponseEntity<Void> deleteById(@PathVariable int userId) {
+        Result<User> result = service.deleteById(userId);
+        if (result.getType() == ResultType.SUCCESS) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
